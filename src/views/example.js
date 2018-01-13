ba95da08c962af05467bd569bdf40ae86e0444e9
Laurence Roberts <lsjroberts@outlook.com>
2018-01-13 22:40:25 +0100

    Delete example view

--- src/views/example.js
+++
@@ -1,6 +0,0 @@
-import { el } from "../react-style-elements/elements";
-import { padding } from "../react-style-elements/elements/attributes";
-
-import { Styles } from "../styles";
-
-export default label => el(Styles.Example, [], `Hello, ${label}!`);
